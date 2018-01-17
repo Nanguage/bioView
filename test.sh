4037b405fa522ddc3e7e84db1982890a00fc95cc
nanguage <nanguage@yahoo.com>
2018-01-17 16:28:08 +0800

    add LICENSE and modify test script

--- test.sh
+++ test.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
-for nimf in `ls src`;
-do
-    if [ $nimf == 'main.nim' ] || [ -d src/$nimf ] ; then
-        continue
-    fi
+# usage:
+#   ./test.sh <moudle-name>
+#   ./test.sh
+#
+# e.g. 
+# ./test.sh configs     # test configs.nim moudle
+# ./test.sh             # test all moudles
+
+function test {
+    moudle=$1
+    moudle_path=src/$moudle.nim
 
     echo "============================="
-    echo test src/$nimf
+    echo test $moudle_path
     echo "============================="
 
-    nim c -r src/$nimf
+    nim c -r $moudle_path
+    exe="src/$moudle"
+    rm $exe
+}
 
-    exe=$(echo $nimf | sed 's/.nim$//')
-    rm src/$exe
-    
-done
+function test_all {
+    for nimf in `ls src`;
+    do
+        if [ $nimf == 'main.nim' ] || [ -d src/$nimf ] ; then
+            continue
+        fi
+
+        moudle=$(echo $nimf | sed 's/.nim$//')
+
+        test $moudle
+
+    done
+}
+
+if [ $# -eq 0 ]; then
+    test_all
+else
+    moudle=$1
+    test $moudle
+fi
